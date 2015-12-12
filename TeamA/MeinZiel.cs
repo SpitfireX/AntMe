4cf9476faf637bfb1f14b621d27312746d705f9b
dane06 <zero111@freenet.de>
2015-12-12 21:41:11 +0100

    KoordinatenKlasse und ZielKlasse hinzugefügt

---
+++ TeamA/MeinZiel.cs
@@ -0,0 +1,21 @@
+﻿using AntMe.Deutsch;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AntMe.Player.TeamA
+{
+    class MeinZiel
+    {
+        public Spielobjekt ziel { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public MeinZiel()
+        {
+            ziel = null;
+        }
+
+    }
+}
---
+++ TeamA/MeineKoordinaten.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AntMe.Player.TeamA
+{
+    class MeineKoordinaten
+    {
+        public int X { get; private set; }      // X-Achse in kartesischen Koordinaten
+        public int Y { get; private set; }      // Y-Achse in kartesischen Koordinaten
+
+        public MeineKoordinaten(int dist, int wink)
+        {
+            X = (int)(dist * Math.Cos(Math.PI * wink / 180.0));
+            Y = (int)(wink * Math.Sin(Math.PI * wink / 180.0));
+        }
+
+        public MeineKoordinaten(int information)
+        {
+            sbyte y = (sbyte)(information % 256);
+            sbyte x = (sbyte)((information >> 8) % 256);
+            X = x * 6;
+            Y = y * 6;
+        }
+
+        /// <summary>
+        /// bestimmt die neue aktuelle Position aus den Polarkoordinaten
+        /// </summary>
+        /// <param name="dist">Entfernung</param>
+        /// <param name="wink">winkel</param>
+        public void SetzeNeueKoordinaten(int dist, int wink)
+        {
+            X = (int)(dist * Math.Cos(Math.PI * wink / 180.0));
+            Y = (int)(wink * Math.Sin(Math.PI * wink / 180.0));
+        }        
+    }
+}
